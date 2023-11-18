7695eade532fcab8dbe516d19d8d1988ab9c973e
lakrestofer <lakrestofer@gmail.com>
2023-11-18 17:18:48 +0100

    update hyprland setup to function on nvidia

--- .local/bin/Hyprland-run.sh
+++ .local/bin/Hyprland-run.sh
@@ -5,4 +5,16 @@ export QT_QPA_PLATFORM="wayland;xcb"
 export SDL_VIDEODRIVER=wayland
 export MOZ_ENABLE_WAYLAND=1 firefox
 export XDG_CURRENT_DESKTOP=Unity
+export XDG_SESSION_TYPE=wayland
+
+# we check if the nvidia kernel module is loaded
+dkms status | grep "nvidia" &> /dev/null
+
+if [ $? -eq 0 ]; then
+  export LIBVA_DRIVER_NAME=nvidia
+  export GBM_BACKEND=nvidia-drm
+  export __GLX_VENDOR_LIBRARY_NAME=nvidia
+  export WLR_NO_HARDWARE_CURSORS=1
+fi
+
 exec Hyprland
