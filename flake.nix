fcb671d9f683e87da2248927e531662c50fe06b0
lakrestofer <lakrestofer@gmail.com>
2024-11-03 13:34:17 +0100

    update conf

--- flake.nix
+++ flake.nix
@@ -8,9 +8,11 @@
       url = "git+https://github.com/kmonad/kmonad?submodules=1&dir=nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, kmonad, nixpkgs, ... } @ inputs:
+  outputs = { self, kmonad, home-manager, nixpkgs, ... } @ inputs:
   let
     system = "x86_64-linux";
   in
@@ -21,6 +23,14 @@
       modules = [
         ./configuration.nix # base configuration
         kmonad.nixosModules.default
+        home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.fincei = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
       ];
     };
 
