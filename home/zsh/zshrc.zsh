456be03545289ff7924a2840832b1be1d9427802
lakrestofer <lakrestofer@gmail.com>
2024-11-11 22:54:49 +0100

    45 current  2024-11-11 00:20:32  24.11.20241029.807e915   6.6.58                          *

--- home/zsh/zshrc.zsh
+++ home/zsh/zshrc.zsh
@@ -12,14 +12,6 @@ bindkey -e # emacs bindings in terminal
 # enable completion
 autoload -Uz compinit; compinit
 
-# Load plugin manager zinit
-ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
-[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
-[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
-source "${ZINIT_HOME}/zinit.zsh"
-autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
-
 # aliases
 alias \
     cp="cp -iv" \
@@ -30,17 +22,5 @@ alias \
     e="hx" \
     z="zathura"
 
-
 # system plugins
 eval "$(zoxide init zsh --cmd cd)"
-
-# plugins loaded with zinit
-zinit light zsh-users/zsh-autosuggestions
-zinit ice as"command" from"gh-r" \
-          atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
-          atpull"%atclone" src"init.zsh"
-zinit light starship/starship
-# zinit ice pick"async.zsh" src"pure.zsh"
-# zinit light sindresorhus/pure # better promt
-zinit wait lucid light-mode for lukechilds/zsh-nvm # node version manager
-zinit light zsh-users/zsh-syntax-highlighting
