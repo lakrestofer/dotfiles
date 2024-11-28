0125eb2cc7bc35babe1039d0b22e611f4b136e6e
lakrestofer <lakrestofer@gmail.com>
2024-11-28 23:40:46 +0100

    52 current  2024-11-28 23:25:26  24.11.20241029.807e915   6.6.58                          *

--- configuration.nix
+++ configuration.nix
@@ -120,7 +120,7 @@
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    programs.hyprland.withUWSM  = true;
+    withUWSM  = true;
   };
   programs.uwsm = {
     enable = true;
