f6854befd0687ad5b8134854ad64b01e1a973106
lakrestofer <lakrestofer@gmail.com>
2024-11-28 23:50:51 +0100

    bump versions

--- configuration.nix
+++ configuration.nix
@@ -120,7 +120,6 @@
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    withUWSM  = true;
   };
   programs.uwsm = {
     enable = true;
