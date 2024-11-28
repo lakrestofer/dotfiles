3b10f046b2547ce85821ef401d34a6ad4ef86ec5
lakrestofer <lakrestofer@gmail.com>
2024-11-28 23:06:20 +0100

    add helix flake

--- flake.nix
+++ flake.nix
@@ -3,13 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # hyprland stuff
     hyprland.url = "github:hyprwm/Hyprland";
     hyprpaper.url = "github:hyprwm/hyprpaper";
     hyprpolkitagent.url = "github:hyprwm/hyprpolkitagent";
+    # kmonad
     kmonad.url = "git+https://github.com/kmonad/kmonad?submodules=1&dir=nix";
     kmonad.inputs.nixpkgs.follows = "nixpkgs";
+    # home manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # some other applications
+    helix.url = "github:helix-editor/helix";
   };
 
   outputs = { self, kmonad, home-manager, nixpkgs, ... } @ inputs:
