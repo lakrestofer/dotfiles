1d428d99ca75551a1aca50452e5a78c3918c0d11
lakrestofer <lakrestofer@gmail.com>
2024-11-28 21:17:41 +0100

    47 current  2024-11-28 21:17:36  24.11.20241029.807e915   6.6.58                          *

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    zed-editor
     wofi
     hyprpaper
     eza
