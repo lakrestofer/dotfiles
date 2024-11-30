68f23b24e45220490ab02fbccea040ca6ff3f058
lakrestofer <lakrestofer@gmail.com>
2024-11-30 23:37:52 +0100

    57 current  2024-11-30 23:37:45  25.05.20241125.4633a7c   6.6.63                          *

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    zathura
     hyprgui
     util-linux
     brightnessctl
