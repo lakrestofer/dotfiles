6a6c946efc29b7dc03ea5973fc45659bbfa6c93e
lakrestofer <lakrestofer@gmail.com>
2024-12-03 21:40:54 +0100

    67 current  2024-12-03 21:40:48  25.05.20241125.4633a7c  6.6.63                          *

--- configuration.nix
+++ configuration.nix
@@ -62,6 +62,8 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    hypridle
+    hyprlock
     ripgrep
     zathura
     hyprgui
