827d73f26085feb253848ad9b398bde8e601e7e7
lakrestofer <lakrestofer@gmail.com>
2024-12-10 23:39:01 +0100

    76 current  2024-12-10 23:38:54  25.05.20241125.4633a7c  6.6.63                          *

--- home/ags/app.ts
+++ home/ags/app.ts
@@ -1,10 +1,9 @@
 import { App } from "astal/gtk3"
-// import style from "./style.scss"
+import style from "./style.scss"
 import Bar from "./widget/Bar"
 
 App.start({
-  // css: style,
-  main() {
-    App.get_monitors().map(Bar)
-  },
+  css: style,
+  instanceName: "js",
+  main: () => App.get_monitors().map(Bar),
 })
