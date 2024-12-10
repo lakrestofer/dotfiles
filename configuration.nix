f5facc36fbb05e8cb95a199695ccd99ed3572b56
lakrestofer <lakrestofer@gmail.com>
2024-12-11 00:26:40 +0100

    78 current  2024-12-11 00:26:34  25.05.20241125.4633a7c  6.6.63                          *

--- configuration.nix
+++ configuration.nix
@@ -100,7 +100,7 @@ in {
       noto-fonts-cjk-sans
       noto-fonts-emoji
       liberation_ttf
-      nerd-fonts.fira-code
+      fira-code
     ];
     fontconfig = {
       useEmbeddedBitmaps = true;
