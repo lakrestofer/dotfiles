402439ff435be7747d7eb6a5a967386f3173e6d0
lakrestofer <lakrestofer@gmail.com>
2024-12-22 13:04:24 +0100

    97 current  2024-12-22 13:04:17  25.05.20241125.4633a7c  6.6.63                          *

--- home/ags/widget/Audio.tsx
+++ home/ags/widget/Audio.tsx
@@ -1,19 +1,39 @@
 import { bind, Variable } from "astal"
 import Wp from "gi://AstalWp"
 
+const icons = ["婢", "奄", "奔", "墳"];
+
+
+function percentage_to_icon(p_: number) {
+  let p = Math.floor(p_ * 100);
+  if (p == 0) return icons[0];
+  if (0 < p && p <= 33) return icons[1];
+  if (33 <= p && p <= 66) return icons[2];
+  return icons[3];
+}
+
+
 export default function Audio() {
+  const show_icon = Variable(true);
+  const toggle_show_icon = () => show_icon.set(!show_icon.get())
+
   const speaker = Wp.get_default()?.audio.defaultSpeaker!
   const speaker_binding = Variable.derive(
-    [bind(speaker, "volume"), bind(speaker, "mute")],
-    (v, m) => {
-      if (m) return "Vol: mute";
-      return `Vol: ${Math.floor(v * 100)}%`;
+    [bind(show_icon), bind(speaker, "volume"), bind(speaker, "mute")],
+    (i, v, m) => {
+      if (i) {
+        if (m) return icons[0];
+        return percentage_to_icon(v);
+      } else {
+        if (m) return "Vol: mute";
+        return `Vol: ${Math.floor(v * 100)}%`;
+      }
     }
   );
 
   return (
-    <box>
-      <label label={bind(speaker_binding).as(v => v)} />
-    </box>
+    <button onClick={toggle_show_icon}>
+      <label label={bind(speaker_binding)} />
+    </button>
   );
 }
