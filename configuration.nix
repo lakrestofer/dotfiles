142618f38405db69e59102e8758b3e97ff91d564
lakrestofer <lakrestofer@gmail.com>
2024-12-26 19:18:01 +0100

    feat: add taplo language server

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,7 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    taplo
     nixfmt-rfc-style
     nil
     nvd
