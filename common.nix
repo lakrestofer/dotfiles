30809b9e659d44594ec6393044ff69de80cc0951
lakrestofer <lakrestofer@gmail.com>
2024-12-28 21:11:38 +0100

    add datni

--- common.nix
+++ common.nix
@@ -62,6 +62,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    inputs.datni.packages.${pkgs.system}.default
     taplo
     nixfmt-rfc-style
     nil
