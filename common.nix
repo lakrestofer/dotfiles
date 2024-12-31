782a2e5fca822a33302fa829cedf70214bb5d872
lakrestofer <lakrestofer@gmail.com>
2024-12-31 03:14:09 +0100

    feat: add jq

--- common.nix
+++ common.nix
@@ -75,6 +75,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    jq
     taskwarrior-tui
     taskwarrior3
     zed-editor
