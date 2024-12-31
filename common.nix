54eecc4d2c146c66c733ec5741c69ab015231767
lakrestofer <lakrestofer@gmail.com>
2024-12-31 03:17:40 +0100

    fix: add jo

--- common.nix
+++ common.nix
@@ -75,6 +75,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    jo
     jq
     taskwarrior-tui
     taskwarrior3
