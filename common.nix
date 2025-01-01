bcf7c75b1f6b81adfd27b48cbb5be37fa73ec58b
lakrestofer <lakrestofer@gmail.com>
2025-01-01 18:37:15 +0100

    feat: add .local/bin to path

--- common.nix
+++ common.nix
@@ -72,6 +72,7 @@ in
       package = pkgs.nix-direnv;
     };
   };
+  environment.localBinInPath = true;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
--- home/scripts/scripts/notes.sh
+++ home/scripts/scripts/notes.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 # opens a new terminal with the interactive zk search feature
 cd ~/Documents/notes
 zk edit --interactive
