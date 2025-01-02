f4a35d8f01a8772e3bdc4f0f9458da4ca474e994
lakrestofer <lakrestofer@gmail.com>
2025-01-02 20:47:01 +0100

    feat: add dprint

--- common.nix
+++ common.nix
@@ -76,6 +76,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    dprint
     inputs.ghostty.packages.${pkgs.system}.default
     wget
     fzf
