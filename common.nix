64e4f4ed33fbb5b456580d179decaa960c56e440
lakrestofer <lakrestofer@gmail.com>
2025-01-04 12:52:07 +0100

    feat: add imagemagic tool

--- common.nix
+++ common.nix
@@ -83,6 +83,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    imagemagick_light
     dprint
     wget
     fzf
