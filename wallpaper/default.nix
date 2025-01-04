7e0c592ead83a584d42a0121cd69715ae3ef5533
lakrestofer <lakrestofer@gmail.com>
2025-01-04 13:23:40 +0100

    feat: direct link wallpaper instead

--- wallpaper/default.nix
+++ wallpaper/default.nix
@@ -1,7 +1,12 @@
 {
+  config,
   ...
 }:
+let
+  wallpaperRoot = "${config.home.homeDirectory}/dotfiles/wallpaper";
+  linkConf = config.lib.file.mkOutOfStoreSymlink;
+in
 {
-  home.file."wallpaper.png".source = ./nixos_flake.png;
-  # home.file."wallpaper.png".source = ./artificial-brain.png;
+  home.file."wallpaper.png".source = linkConf "${wallpaperRoot}/nixos_flake.png";
+  # home.file."wallpaper.png".source = linkConf "${wallpaperRoot}/artificial-brain.png";
 }
