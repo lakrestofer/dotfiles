5a02058e0b33e46018f07b5621759da9f4c33710
lakrestofer <lakrestofer@gmail.com>
2025-01-10 13:39:05 +0100

    remove walker conf

--- home.nix
+++ home.nix
@@ -52,15 +52,6 @@ in
     runAsService = true;
 
     # All options from the config.json can be used here.
-    config = {
-      search.placeholder = ">";
-      ui.fullscreen = true;
-      list = {
-        height = 200;
-      };
-      websearch.prefix = "?";
-      switcher.prefix = "/";
-    };
 
     # If this is not set the default styling is used.
   };
