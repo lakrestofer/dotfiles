dfff6ef06be28b9bfe238c1e33a00a2263761572
lakrestofer <lakrestofer@gmail.com>
2025-01-12 14:16:08 +0100

    feat: add monitor config to minji hyprland conf

--- hosts/minji/home.nix
+++ hosts/minji/home.nix
@@ -11,6 +11,10 @@
   # minji specific hyprland config
   # 3 monitors with set sizes
   wayland.windowManager.hyprland.settings = {
-
+    monitor = [
+      "HDMI-A-1,highres,-1080x-240,auto,transform,3"
+      "DP-1,highres,0x0,auto"
+      "DP-2,highres,2560x-240,auto,transform,1"
+    ];
   };
 }
