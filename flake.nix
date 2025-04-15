ad1fc270853def76b863fc981c15459a78027c7b
lakrestofer <lakrestofer@gmail.com>
2025-04-15 08:46:21 +0200

    use the generic nixosModule from nixos-hardware

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
             ./hosts/selbeiskami # thinkpad x220 specific configuration
             ./common.nix # base configuration
             kmonad.nixosModules.default
-            nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen2
+            nixos-hardware.nixosModules.lenovo-thinkpad-t14
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
