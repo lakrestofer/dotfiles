b8cf0719e67abcc096f5e6149ce5123583f6dc0d
lakrestofer <lakrestofer@gmail.com>
2025-05-17 17:35:12 +0200

    break out output to its own file

---
+++ outputs/default.nix
@@ -0,0 +1,103 @@
+{
+  kmonad,
+  home-manager,
+  nixpkgs,
+  nixos-hardware,
+  ...
+}@inputs:
+let
+  system = "x86_64-linux";
+  pkgs = import nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+in
+{
+  nixosConfigurations = {
+    amanda = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {
+        inherit inputs;
+        inherit pkgs;
+      };
+      modules = [
+        ./hosts/amanda # thinkpad x220 specific configuration
+        ./common.nix # base configuration
+        kmonad.nixosModules.default
+        nixos-hardware.nixosModules.lenovo-thinkpad-x220
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.fincei = import ./home.nix;
+          home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = {
+            inherit inputs;
+          };
+        }
+      ];
+    };
+    minji = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {
+        inherit inputs;
+        inherit pkgs;
+      };
+      modules = [
+        ./hosts/minji # desktop
+        ./common.nix # base configuration
+        home-manager.nixosModules.home-manager
+        nixos-hardware.nixosModules.common-cpu-amd
+        nixos-hardware.nixosModules.common-cpu-amd-pstate
+        nixos-hardware.nixosModules.common-gpu-amd
+        nixos-hardware.nixosModules.common-hidpi
+        nixos-hardware.nixosModules.common-pc-ssd
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.fincei = import ./hosts/minji/home.nix;
+          home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = {
+            inherit inputs;
+          };
+        }
+      ];
+    };
+    selbeiskami = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {
+        inherit inputs;
+        inherit pkgs;
+      };
+      modules = [
+        ./hosts/selbeiskami # thinkpad x220 specific configuration
+        ./common.nix # base configuration
+        kmonad.nixosModules.default
+        nixos-hardware.nixosModules.lenovo-thinkpad-t14
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.fincei = import ./home.nix;
+          home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = {
+            inherit inputs;
+          };
+        }
+      ];
+    };
+    nucbox = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {
+        inherit inputs;
+        inherit pkgs;
+      };
+      modules = [ ./hosts/nucbox ];
+    };
+  };
+  packages.${system} = { };
+  # === shells ===
+  # devShells.${system} = {
+  #   # default = pkgs.mkShell { };
+  # };
+}
