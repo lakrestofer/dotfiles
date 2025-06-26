6017b7eae0f03b4f957b61b1713c21bec1cea007
lakrestofer <lakrestofer@gmail.com>
2025-06-26 09:43:32 +0200

    remove ags

--- home/ags/app.ts
+++
@@ -1,9 +0,0 @@
-import { App } from "astal/gtk3"
-import style from "./style.scss"
-import Bar from "./widget/Bar"
-
-App.start({
-  css: style,
-  instanceName: "js",
-  main: () => App.get_monitors().map(Bar),
-})
