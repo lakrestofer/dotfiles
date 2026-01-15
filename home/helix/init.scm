8d048f7b3485988f2349dbe2aa875230662727b8
lakrestofer <lakrestofer@gmail.com>
2026-01-15 18:23:05 +0100

    some initial steel configuration for helix

---
+++ home/helix/init.scm
@@ -0,0 +1,10 @@
+(require "helix/configuration.scm")
+
+(require "cogs/focus.scm")
+(require (only-in "helix/ext.scm" evalp eval-buffer))
+
+(define-lsp "steel-language-server" (command "steel-language-server") (args '()))
+
+(define-language "scheme"
+                 (language-servers '("steel-language-server")))
+
