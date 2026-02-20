651664050f7d6085c2d89fed6f544e4e86c8b694
lakrestofer <lakrestofer@gmail.com>
2026-02-20 12:50:50 +0100

    add home-manager integration into flake-parts

--- flake.nix
+++ flake.nix
@@ -16,7 +16,10 @@
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ (inputs.import-tree ./modules) ];
+      imports = [
+        (inputs.import-tree ./modules)
+        inputs.home-manager.flakeModules.home-manager
+      ];
       systems = [
         "x86_64-linux"
       ];
